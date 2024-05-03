d5cc3ee2fa41505d708523fd2f93cd994c347760
Fathy <abdelrahmanfathe95@gmail.com>
2024-05-03 21:11:42 +0200

    add julia file for hw02

---
+++ HW02/newmark..jl
@@ -0,0 +1,11 @@
+### A Pluto.jl notebook ###
+# v0.17.3
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ e3dbdee8-7f62-4301-b848-6393954121a8
+i'm new here!
+
+# ╔═╡ Cell order:
+# ╠═e3dbdee8-7f62-4301-b848-6393954121a8
