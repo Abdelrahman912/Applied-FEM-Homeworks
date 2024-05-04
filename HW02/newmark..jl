ac6ae4eb099ab9e2e1c73bda95d3cb1348154249
Fathy <abdelrahmanfathe95@gmail.com>
2024-05-04 15:12:36 +0200

    add task 3

--- HW02/newmark..jl
+++ HW02/newmark..jl
@@ -8,10 +8,15 @@ using InteractiveUtils
 begin
 	import Pkg
 	Pkg.activate(".")
+	Pkg.add("Plots")
 end
 
 # ╔═╡ c5bf3248-2ff1-4257-ba34-8b322b667abb
-using LinearAlgebra
+begin
+	using LinearAlgebra
+	using Plots
+	using LaTeXStrings
+end
 
 # ╔═╡ 6075d462-d1ff-4b30-9de8-2f7a1eb9ecdf
 md"""#### Task 1:
@@ -189,6 +194,7 @@ struct DynamicResponse
 	u::Matrix{Float64}
 	ud::Matrix{Float64}
 	udd::Matrix{Float64}
+	t::Vector{Float64}
 end
 
 # ╔═╡ b1057602-76fe-469c-b382-05c65fb14e60
@@ -196,6 +202,7 @@ struct Error
 	e_abs::Vector{Float64}
 	η::Vector{Float64}
 	e_cum::Vector{Float64}
+	t::Vector{Float64}
 end
 
 # ╔═╡ 3cea61f0-bc04-4092-8e3b-7803932865d5
@@ -254,33 +261,52 @@ function generalized_alpha(tf, α₁, α₂, ρ_∞)
 		e_cum[i+1] = sum(e_abs)
 	end
 
-	(response = DynamicResponse(u,ud,udd), errors = Error(e_abs,η,e_cum))
+	(response = DynamicResponse(u,ud,udd,t), errors = Error(e_abs,η,e_cum,t))
 end
 
 # ╔═╡ 9d7e83e5-4d75-4d96-913f-a656d9866735
 generalized_alpha(5.0, 1.0, 0.0, 1.0)
 
 # ╔═╡ e0ced7bc-baae-4c15-8b6e-9e50be04ced9
-isbitstype(Response)
+md"""#### Task 3a: 
+Given: $t_{final} = 5 \space s, α_1 = 1, α_2 = 0, ρ_∞ = 1.0$ 
+"""
 
 # ╔═╡ f27a727f-f80c-4dd2-bdef-63c7f3ca587f
-b = [1 ,2 ]
+resp_a , err_a = generalized_alpha(5.0, 1.0, 0.0, 1.0)
 
 # ╔═╡ 2a801b3c-49a3-4aa7-be75-452711c4d4a0
-norm(b)
+begin
+	plot(resp_a.t,resp_a.u[1,:],title="Displacement response with time (Task 3a)",xlabel=L"time",ylabel=L"displacement",label=L"$\theta$")
+	plot!(resp_a.t,resp_a.u[2,:],label=L"u")
+
+end
 
 # ╔═╡ 20193589-4189-4347-ac5d-0e4370152ce6
-inv(M)
+md"""#### Task 3b: 
+Given: $t_{final} = 5 \space s, α_1 = 0, α_2 = 0, ρ_∞ = 0.1$ 
+"""
 
 # ╔═╡ 7cb8658f-9a03-4807-8e18-3775b3f675aa
-inv(M)*b
+resp_b , err_b = generalized_alpha(5.0, 0.0, 0.0, 0.1)
 
 # ╔═╡ 4ac90089-eee4-4ce3-a2e8-0152877f4414
-a = [1 2 ; 3 4]
+begin
+	plot(resp_b.t,resp_b.u[1,:],title="Displacement response with time (Task 3b)",xlabel=L"time",ylabel=L"displacement",label=L"$\theta$")
+	plot!(resp_b.t,resp_b.u[2,:],label=L"u")
+end
 
-# ╔═╡ 9fa61850-7570-48b2-863b-1f6cca64b684
-a[:,1] + 
-a[:,2]
+# ╔═╡ 5309e3a3-ab35-4cc4-88cc-119813ab62a5
+md"""#### Task3 (Explanation):
+##### Observtion:
+In *Task 3a* **both** degree of freedoms (i.e. $u$ & $θ$) are being damped with time, whereas in *Task 3b* **only $u$** is being damped with time and the amplitude of $\theta$ is constant through time.
+
+##### Explanation:
+The reason behind such observation is that $α_1$ & $α_2$ are called **physical damping parameters** because they contribute to the formulation of the *Rayleigh damping* (i.e. $\boldsymbol{C} = α_1 \boldsymbol{M} + α_2 \boldsymbol{K}$) and as given in *Task 3a* $α_1 \neq 0 → C \neq 0$, consequently, all degree of freedoms are being damped by $\boldsymbol{C}$ regardless their frequencies. \
+
+However, $ρ_∞$ is called **numerical damping parameter** due to the numerical error arised from this parameter that leads to damping. Additionally, it only damps degree of freedoms that has higher frequencies and this obvious in *task 3b* which has **no physical** damping but has **numerical** damping. \
+Finaly, in *task 3a* there is no numerical damping because, $γ = \frac{1}{2} → ρ_∞ = 1$ (slides pg. 59) and numerical damping only occurs if $γ > \frac{1}{2} → ρ_∞ < 1$
+"""
 
 # ╔═╡ Cell order:
 # ╠═e3dbdee8-7f62-4301-b848-6393954121a8
@@ -294,10 +320,10 @@ a[:,2]
 # ╠═b1057602-76fe-469c-b382-05c65fb14e60
 # ╠═3cea61f0-bc04-4092-8e3b-7803932865d5
 # ╠═9d7e83e5-4d75-4d96-913f-a656d9866735
-# ╠═e0ced7bc-baae-4c15-8b6e-9e50be04ced9
+# ╟─e0ced7bc-baae-4c15-8b6e-9e50be04ced9
 # ╠═f27a727f-f80c-4dd2-bdef-63c7f3ca587f
 # ╠═2a801b3c-49a3-4aa7-be75-452711c4d4a0
-# ╠═20193589-4189-4347-ac5d-0e4370152ce6
+# ╟─20193589-4189-4347-ac5d-0e4370152ce6
 # ╠═7cb8658f-9a03-4807-8e18-3775b3f675aa
-# ╠═4ac90089-eee4-4ce3-a2e8-0152877f4414
-# ╠═9fa61850-7570-48b2-863b-1f6cca64b684
+# ╟─4ac90089-eee4-4ce3-a2e8-0152877f4414
+# ╠═5309e3a3-ab35-4cc4-88cc-119813ab62a5
