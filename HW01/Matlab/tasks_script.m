72f4cc9482b8d1adb4acca3ffcd6a9442e307ed0
Abdelrahman Fathy (Tee) <abdelrahmanfathe95@gmail.com>
2024-05-31 02:51:32 +0200

    add reports

--- HW01/Matlab/tasks_script.m
+++ HW01/Matlab/tasks_script.m
@@ -1,6 +1,10 @@
 clc;
 clear;
 
+fprintf('Homework 1: Equations of motion and eigenvalue problem \n');
+fprintf('Name: Abdelrahman Fathy Abdelhaleem Aly Abdelrahman \n');
+fprintf('Matr.-Nr.: 108023251500 \n \n');
+
 m =25.0 ;
 M_ = 700.0;
 k = 3e3;
@@ -15,13 +19,12 @@ fprintf('Mass Matrix: \n');
 disp(M);
 TOL = 1e-6; % Convergence tolerence.
 
-
 % Task 7:
-
 x1 = [1;0]; % initial guess for Task 7.
 
 % Froward Iteration
 [lambda, phi, n ]= forward_iter(K,M,x1,TOL);
+fprintf('Task 7: \n');
 fprintf('Forward Iteration: \n');
 fprintf('eigen value: %f \n',lambda) % 17.8 -> (largest eigen value)
 fprintf('eigen vector: [%f, %f] \n',phi(1),phi(2)) % [0.7071; -0.7071]
@@ -34,10 +37,10 @@ fprintf('eigen value: %f \n',lambda) % 9.8 -> (smallest eigen value)
 fprintf('eigen vector: [%f, %f] \n',phi(1),phi(2)) % [0.7071; 0.7071]
 fprintf('number of iterations: %d \n \n',n) % 13
 
-
 % Task 8:
 x2 = [1;1]; % initial guess for Task 8.
 [lambda, phi, n ]= forward_iter(K,M,x2,TOL);
+fprintf('Task 8: \n');
 fprintf('Forward Iteration with initial guess [%f, %f]: \n',x2(1),x2(2));
 fprintf('eigen value: %f \n',lambda) % 9.8 -> (smallest eigen value)
 fprintf('eigen vector: [%f, %f] \n',phi(1),phi(2)) % [0.7071; 0.7071]
